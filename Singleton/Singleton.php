c01efcb27e589da674f79f7460cb996828bd8961
Marcos Alemany <marcos.alemany.m@gmail.com>
2024-06-12 15:35:00 +0200

    Patron Singleton

---
+++ Singleton/Singleton.php
@@ -0,0 +1,23 @@
+<?php
+class Singleton
+{
+    private static $instance = null;
+    
+    // El constructor es privado para evitar que se pueda instanciar la clase
+    private function __construct()
+    {
+        
+    }
+    
+    // Metodo para obtener la instancia de la clase, se creara una unica vez si la instancia es null
+    public static function getInstance()
+    {
+        if (self::$instance == null) 
+        {
+            self::$instance = new Singleton();
+        }
+        
+        return self::$instance;
+    }
+}
+?> 
---
+++ Singleton/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario de Acciones</title>
+</head>
+<body>
+
+<h2> Elige el tipo de casa</h2>
+
+<form method="post" action="index.php">
+    <button type="submit" name="action" value="action1">Instanciar</button>
+</form>
+
+<?php
+
+require_once "Singleton.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $singleton = Singleton::getInstance();
+
+    // Para verificar que la instancia es la misma, puedes hacer lo siguiente:
+    var_dump($singleton);
+}
+?>
+
+</body>
+</html>
